a3130bb190b9eab6b26a1304c6fe722363e5dfe5
Bogdans Afonins <bogdans.afonins@thethingsindustries.com>
2019-05-21 20:46:00 +0200

    console: Adjust application sub views

--- pkg/webui/console/views/application-general-settings/index.js
+++ pkg/webui/console/views/application-general-settings/index.js
@@ -32,6 +32,8 @@ import diff from '../../../lib/diff'
 import toast from '../../../components/toast'
 import SubmitBar from '../../../components/submit-bar'
 
+import { selectSelectedApplication } from '../../store/selectors/application'
+
 import api from '../../api'
 
 const m = defineMessages({
@@ -51,9 +53,9 @@ const validationSchema = Yup.object().shape({
 })
 
 
-@connect(function ({ application }) {
+@connect(function (state) {
   return {
-    application: application.application,
+    application: selectSelectedApplication(state),
   }
 })
 @withBreadcrumb('apps.single.general-settings', function (props) {
